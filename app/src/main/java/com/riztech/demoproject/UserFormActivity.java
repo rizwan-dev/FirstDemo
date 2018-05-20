ba5948ed34f3ed413bb601fb4a6e26a6e4c6c7c1
Rizwanul Haque <rizwan@relfor.com>
2018-05-20 11:58:50 +0530

    Added design files

---
+++ app/src/main/java/com/riztech/demoproject/UserFormActivity.java
@@ -0,0 +1,13 @@
+package com.riztech.demoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_form);
+    }
+}
